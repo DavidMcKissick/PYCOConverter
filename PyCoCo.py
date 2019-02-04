279ecfe4532aba748b2ed63dda0a6229845d675b
David McKissick <dwm0816@outlook.com>
2019-02-03 17:13:16 -0800

    Updated naming conventions to fit PEP8

--- PyCoCo.py
+++ PyCoCo.py
@@ -25,194 +25,194 @@ cup = 236.588
 li = 1000
 gal = 3785.41178
 #End material measurements_________________
-def lb():
+def line_break():
 	print("_______________________________________________________________________________________________________________")
 	print("")
 
-def intro():
-	lb()
+def app_introduction():
+	line_break()
 	print("Welcome")
 	print("v"+str(version))
 	print("Type 'help' for a list of commands, type units for a list of units, type exit to quit.")
 
-def main():	
-	lb()
-	UInput = input("What would you like to do: ")
-	if UInput == "help":
-		lb()
+def app_main():	
+	line_break()
+	user_input = input("What would you like to do: ")
+	if user_input == "help":
+		line_break()
 		print("'length' or 'l': do length measurement conversion")
 		print("'volumetric' or 'volume' or 'v': do volumetric conversions")
 		print("'Temperature' or 'temp' or 't': do temperature conversions")
 		print("'help': Get help")
 		print("'units': Print a list of units")
 		print("'exit': quit the software or function")
-		main()
-	elif UInput == "units":
-		lb()
+		app_main()
+	elif user_input == "units":
+		line_break()
 		print("Millimeter, Centimeter, Meter, Kilometer, Inch, Foot, Yard, mile")
 		print("Milliliter, Centiliter, Teaspoon, Tablespoon, Fluid ounce, cup, liter, gallon")
-		main()
-	elif UInput == "exit":
+		app_main()
+	elif user_input == "exit":
 		return
-	elif UInput == "length" or UInput == "l":
-		lengthHandler()
-	elif UInput == "volumetric" or UInput == "volume" or UInput == "v":
-		volumeHandler()
-	elif UInput == "temperature" or UInput == "temp" or UInput == "t":
-		tempHandler()
+	elif user_input == "length" or user_input == "l":
+		length_handler()
+	elif user_input == "volumetric" or user_input == "volume" or user_input == "v":
+		volume_handler()
+	elif user_input == "temperature" or user_input == "temp" or user_input == "t":
+		temperature_handler()
 	else:
 		print("I'm sorry, I do not currently understand that argument.")
-		main()
+		app_main()
 
 #Universal handler function
-def measureParser(a,b,measure,MeasureLength): #Calculates the request and prints the final outcome
-	a = a[:-MeasureLength]
+def measure_parser(a,b,measure,measure_length): #Calculates the request and prints the final outcome
+	a = a[:-measure_length]
 	aVal = globals()[measure] * int(a)
 	bVal = globals()[b]
 	outcome = aVal / bVal
 	print(a + measure + " in " + b + " is: " + str(outcome) + b)
 	time.sleep(0.4)
-	main()
+	app_main()
 
 #Length handling block start
-def lengthCalc(a, b): #Takes in user input then passes it to the calculation function
-	lb()
+def length_calculator(a, b): #Takes in user input then passes it to the calculation function
+	line_break()
 	if a.endswith("mm"):
 		try:
-			measureParser(a, b, "mm", 2)
+			measure_parser(a, b, "mm", 2)
 		except:
 			print("The target unit was not correct.")
-			lengthHandler()
+			length_handler()
 	elif a.endswith("cm"):
 		try:
-			measureParser(a, b, "cm", 2)
+			measure_parser(a, b, "cm", 2)
 		except:
 			print("The target unit was not correct.")
-			lengthHandler()
+			length_handler()
 	elif a.endswith("me"):
 		try:
-			measureParser(a, b, "me", 2)
+			measure_parser(a, b, "me", 2)
 		except:
 			print("The target unit was not correct.")
-			lengthHandler()
+			length_handler()
 	elif a.endswith("km"):
 		try:
-			measureParser(a, b, "km", 2)
+			measure_parser(a, b, "km", 2)
 		except:
 			print("The target unit was not correct.")
-			lengthHandler()
+			length_handler()
 	elif a.endswith("inch"): #unfortunately "in" is a reserved word
 		try:
-			measureParser(a, b, "in", 2)
+			measure_parser(a, b, "in", 2)
 		except:
 			print("The target unit was not correct.")
-			lengthHandler()
+			length_handler()
 	elif a.endswith("ft"):
 		try:
-			measureParser(a, b, "ft", 2)
+			measure_parser(a, b, "ft", 2)
 		except:
 			print("The target unit was not correct.")
-			lengthHandler()
+			length_handler()
 	elif a.endswith("yd"):
 		try:
-			measureParser(a, b, "yd", 2)
+			measure_parser(a, b, "yd", 2)
 		except:
 			print("The target unit was not correct.")
-			lengthHandler()
+			length_handler()
 	elif a.endswith("mi"):
 		try:
-			measureParser(a, b, "mi", 2)
+			measure_parser(a, b, "mi", 2)
 		except:
 			print("The target unit was not correct.")
-			lengthHandler()
+			length_handler()
 	else:
 		print("I'm sorry, I don't understand that measurement")
-		main()
+		app_main()
 
 
-def lengthHandler(): #Presents the options available for length and asks for input, also accepts exit.
-	lb()
+def length_handler(): #Presents the options available for length and asks for input, also accepts exit.
+	line_break()
 	print('Viable measurements: mm, cm, m, km, inch, ft, yd, mi')
 	print("Format queries as number then measure, eg: 50mm")
-	firstInp = input("Measure to convert: ")
-	if firstInp == "exit":
+	first_input = input("Measure to convert: ")
+	if first_input == "exit":
 		return
 	else:
-		secondInp = input("Measure to convert TO (measurements only): ")
-		if secondInp == "exit":
+		second_input = input("Measure to convert TO (measurements only): ")
+		if second_input == "exit":
 			return
 		else:
-			lengthCalc(firstInp, secondInp)
+			length_calculator(first_input, second_input)
 #Length handling block end
 
 #Volume handling block begin
-def volCalc(a,b):
+def volume_calculator(a,b):
 	if a.endswith("ml"):
 		try:
-			measureParser(a,b,"ml",2)
+			measure_parser(a,b,"ml",2)
 		except:
 			print("The target unit was not correct")
-			volumeHandler()
+			volume_handler()
 	elif a.endswith("cl"):
 		try:
-			measureParser(a,b,"cl",2)
+			measure_parser(a,b,"cl",2)
 		except:
 			print("The target unit was not correct")
-			volumeHandler()
+			volume_handler()
 	elif a.endswith("tsp"):
 		try:
-			measureParser(a,b,"tsp",3)
+			measure_parser(a,b,"tsp",3)
 		except:
 			print("The target unit was not correct")
-			volumeHandler()
+			volume_handler()
 	elif a.endswith("tbsp"):
 		try:
-			measureParser(a,b,"tbsp",4)
+			measure_parser(a,b,"tbsp",4)
 		except:
 			print("The target unit was not correct")
-			volumeHandler()
+			volume_handler()
 	elif a.endswith("oz"):
 		try:
-			measureParser(a,b,"oz",2)
+			measure_parser(a,b,"oz",2)
 		except:
 			print("The target unit was not correct")
-			volumeHandler()
+			volume_handler()
 	elif a.endswith("cup"):
 		try:
-			measureParser(a,b,"cup",3)
+			measure_parser(a,b,"cup",3)
 		except:
 			print("The target unit was not correct")
-			volumeHandler()
+			volume_handler()
 	elif a.endswith("li"):
 		try:
-			measureParser(a,b,"li",2)
+			measure_parser(a,b,"li",2)
 		except:
 			print("The target unit was not correct")
-			volumeHandler()
+			volume_handler()
 	elif a.endswith("gal"):
 		try:
-			measureParser(a,b,"gal",3)
+			measure_parser(a,b,"gal",3)
 		except:
 			print("The target unit was not correct")
-			volumeHandler()
+			volume_handler()
 	else:
 		print("I'm sorry, I don't understand that measurement.")
-		main()
+		app_main()
 
 
-def volumeHandler(): #Presents the options available for volume and asks for input, also accepts exit.
-	lb()
+def volume_handler(): #Presents the options available for volume and asks for input, also accepts exit.
+	line_break()
 	print('Viable measurements: ml, cl, tsp, tbsp, oz, cup, li, gal')
 	print("Format queries as number then measure, eg: 5ml")
-	firstInp = input("Measure to convert: ")
-	if firstInp == "exit":
+	first_input = input("Measure to convert: ")
+	if first_input == "exit":
 		return
 	else:
-		secondInp = input("Measure to convert TO (measurements only): ")
-		if secondInp == "exit":
+		second_input = input("Measure to convert TO (measurements only): ")
+		if second_input == "exit":
 			return
 		else:
-			volCalc(firstInp, secondInp)
+			volume_calculator(first_input, second_input)
 #Volume handling block end
 
 
@@ -223,36 +223,36 @@ def tempCalc(type, c, f):
 	if type == 1: #C to F
 		temp = valueC * 1.8 + 32
 		print(str(valueC) + "c in f is: " + str(temp) + "f")
-		main()
+		app_main()
 	elif type == 2: #F to C
 		stepdown = valueF - 32
 		temp = stepdown / 1.8
 		print(str(valueF) + "f in c is: " + str(temp) + "c")
-		main()
+		app_main()
 
-def tempHandler():
+def temperature_handler():
 	print("Lets convert some temps!")
-	lb()
+	line_break()
 	print("1. Celsius to Fahrenheit")
 	print("2. Fahrenheit to Celsius")
 	print("Type exit to go back")
-	lb()
+	line_break()
 	choice = input("What would you like to do: ")
 	if choice == "1":
-		celsiusInp = input("Input temperature: ")
-		tempCalc(1, celsiusInp, "0")
+		celsius_input = input("Input temperature: ")
+		tempCalc(1, celsius_input, "0")
 	elif choice == "2":
-		FahrenInp = input("Input temperature: ")
-		tempCalc(2, "0", FahrenInp)
+		fahrenheit_input = input("Input temperature: ")
+		tempCalc(2, "0", fahrenheit_input)
 	elif choice == "exit":
-		main()
+		app_main()
 	elif choice == "help":
-		lb()
+		line_break()
 		print(">>>>>type the number of your selection to select your function, type exit to return to the main menu.<<<<<")
-		lb()
-		tempHandler()
+		line_break()
+		temperature_handler()
 	else:
 		print("I do not undersand that input")
-		tempHandler()
-intro()
-main()
+		temperature_handler()
+app_introduction()
+app_main()
